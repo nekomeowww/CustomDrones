de9a5111d54b7093a1e5328dd09f98961481e485
Enfu Zhang <fanff0324@gmail.com>
2017-07-24 23:57:50 +0800

    World func and field match to field.csv and methods.csv

--- src/main/java/com/github/nekomeowww/customdrones/worldgen/WorldGenDroneTower.java
+++ src/main/java/com/github/nekomeowww/customdrones/worldgen/WorldGenDroneTower.java
@@ -12,6 +12,7 @@ import net.minecraft.util.WeightedRandom;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import com.github.nekomeowww.customdrones.api.Filters;
 import com.github.nekomeowww.customdrones.api.Filters.FilterReplaceable;
 import com.github.nekomeowww.customdrones.api.helpers.VecHelper;
 import com.github.nekomeowww.customdrones.api.helpers.WorldHelper;
@@ -26,9 +27,9 @@ public class WorldGenDroneTower
     public static void genDroneTower(World world, int posX, int posZ)
     {
         BlockPos startPos = WorldHelper.getLowestAir(world, bp(posX, 0, posZ));
-        if (startPos.func_177956_o() > 12)
+        if (startPos.getY() > 12)
         {
-            IBlockState torch = Blocks.field_150478_aa.func_176223_P();
+            IBlockState torch = Blocks.TORCH.getDefaultState();
             WorldGen.GenBlockEntry blocks = blocksForBiome(world, startPos);
             int surfaceFloors = 3;
             int topDecorHeight = 5;
@@ -36,8 +37,8 @@ public class WorldGenDroneTower
             int halfWidth = 8;
             int maxBulge = 3;
             int minY = 4;
-            int groundY = startPos.func_177956_o();
-            int maxY = groundY + floorHeight * (surfaceFloors + 1) - (startPos.func_177956_o() - minY) % floorHeight;
+            int groundY = startPos.getY();
+            int maxY = groundY + floorHeight * (surfaceFloors + 1) - (startPos.getY() - minY) % floorHeight;
             int minX = posX - halfWidth;
             int maxX = posX + halfWidth;
             int minZ = posZ - halfWidth;
@@ -60,25 +61,25 @@ public class WorldGenDroneTower
                     int tmaxX = maxX + bulge;
                     int tminZ = minZ - bulge;
                     int tmaxZ = maxZ + bulge;
-                    genBoxMargin(world, bp(tminX, tY, tminZ), bp(tmaxX, tY, tmaxZ), blocks.primary, Blocks.field_150350_a
-                            .func_176223_P(), 1, 0, 1, null, null);
-                    world.func_175656_a(bp(tminX, tY, tminZ), blocks.decor1);
-                    world.func_175656_a(bp(tmaxX, tY, tminZ), blocks.decor1);
-                    world.func_175656_a(bp(tmaxX, tY, tmaxZ), blocks.decor1);
-                    world.func_175656_a(bp(tminX, tY, tmaxZ), blocks.decor1);
-                    world.func_175656_a(bp(tminX, tY, posZ), blocks.decor2);
-                    world.func_175656_a(bp(tmaxX, tY, posZ), blocks.decor2);
-                    world.func_175656_a(bp(posX, tY, tminZ), blocks.decor2);
-                    world.func_175656_a(bp(posX, tY, tmaxZ), blocks.decor2);
+                    genBoxMargin(world, bp(tminX, tY, tminZ), bp(tmaxX, tY, tmaxZ), blocks.primary, Blocks.AIR
+                            .getDefaultState(), 1, 0, 1, null, null);
+                    world.setBlockState(bp(tminX, tY, tminZ), blocks.decor1);
+                    world.setBlockState(bp(tmaxX, tY, tminZ), blocks.decor1);
+                    world.setBlockState(bp(tmaxX, tY, tmaxZ), blocks.decor1);
+                    world.setBlockState(bp(tminX, tY, tmaxZ), blocks.decor1);
+                    world.setBlockState(bp(tminX, tY, posZ), blocks.decor2);
+                    world.setBlockState(bp(tmaxX, tY, posZ), blocks.decor2);
+                    world.setBlockState(bp(posX, tY, tminZ), blocks.decor2);
+                    world.setBlockState(bp(posX, tY, tmaxZ), blocks.decor2);
                     if (bulge == maxBulge)
                     {
-                        world.func_175656_a(bp(tminX + 1, tY, tminZ + 1), Blocks.field_150478_aa.func_180642_a(world,
+                        world.setBlockState(bp(tminX + 1, tY, tminZ + 1), Blocks.TORCH.getStateForPlacement(world,
                                 bp(tminX + 1, tY, tminZ + 1), EnumFacing.UP, 0.0F, 0.0F, 0.0F, 0, null));
-                        world.func_175656_a(bp(tmaxX - 1, tY, tminZ + 1), Blocks.field_150478_aa.func_180642_a(world,
+                        world.setBlockState(bp(tmaxX - 1, tY, tminZ + 1), Blocks.TORCH.getStateForPlacement(world,
                                 bp(tmaxX - 1, tY, tminZ + 1), EnumFacing.UP, 0.0F, 0.0F, 0.0F, 0, null));
-                        world.func_175656_a(bp(tmaxX - 1, tY, tmaxZ - 1), Blocks.field_150478_aa.func_180642_a(world,
+                        world.setBlockState(bp(tmaxX - 1, tY, tmaxZ - 1), Blocks.TORCH.getStateForPlacement(world,
                                 bp(tmaxX - 1, tY, tmaxZ - 1), EnumFacing.UP, 0.0F, 0.0F, 0.0F, 0, null));
-                        world.func_175656_a(bp(tminX + 1, tY, tmaxZ - 1), Blocks.field_150478_aa.func_180642_a(world,
+                        world.setBlockState(bp(tminX + 1, tY, tmaxZ - 1), Blocks.TORCH.getStateForPlacement(world,
                                 bp(tminX + 1, tY, tmaxZ - 1), EnumFacing.UP, 0.0F, 0.0F, 0.0F, 0, null));
                     }
                 }
@@ -87,10 +88,10 @@ public class WorldGenDroneTower
                 boolean bigDrone = genDrones(world, bp(minX + 1, fminY + 1, minZ + 1),
                         bp(maxX - 1, fmaxY - 1, maxZ - 1), floorLevel);
                 genChest(world, bp(posX, fminY + 1, posZ), generateLoots(floorLevel + (bigDrone ? 4 : 0)));
-                world.func_175656_a(bp(posX + 1, fminY + 1, posZ), torch);
-                world.func_175656_a(bp(posX - 1, fminY + 1, posZ), torch);
-                world.func_175656_a(bp(posX, fminY + 1, posZ + 1), torch);
-                world.func_175656_a(bp(posX, fminY + 1, posZ - 1), torch);
+                world.setBlockState(bp(posX + 1, fminY + 1, posZ), torch);
+                world.setBlockState(bp(posX - 1, fminY + 1, posZ), torch);
+                world.setBlockState(bp(posX, fminY + 1, posZ + 1), torch);
+                world.setBlockState(bp(posX, fminY + 1, posZ - 1), torch);
             }
             for (int a = 0; a < topDecorHeight; a++) {
                 genBoxMargin(world, bp(minX - a, maxY + a, minZ - a), bp(maxX + a, maxY + a, maxZ + a), blocks.secondary, air, 1, 0, 1, null, new Filters.FilterReplaceable(true));
@@ -125,23 +126,23 @@ public class WorldGenDroneTower
         for (int a = 0; a < count; a++)
         {
             int randomCount = rnd.nextInt(floorLevel + 1);
-            DroneWeightedLists.WeightedItemStack wis = (DroneWeightedLists.WeightedItemStack)WeightedRandom.func_76271_a(rnd, loots);
+            DroneWeightedLists.WeightedItemStack wis = (DroneWeightedLists.WeightedItemStack)WeightedRandom.getRandomItem(rnd, loots);
             for (int b = 0; b < randomCount; b++)
             {
-                DroneWeightedLists.WeightedItemStack randomTo = (DroneWeightedLists.WeightedItemStack)WeightedRandom.func_76271_a(rnd, loots);
-                if ((wis == null) || (randomTo.field_76292_a < wis.field_76292_a)) {
+                DroneWeightedLists.WeightedItemStack randomTo = (DroneWeightedLists.WeightedItemStack)WeightedRandom.getRandomItem(rnd, loots);
+                if ((wis == null) || (randomTo.itemWeight < wis.itemWeight)) {
                     wis = randomTo;
                 }
             }
             if (wis != null)
             {
-                ItemStack is = wis.is.func_77946_l();
+                ItemStack is = wis.is.copy();
                 int minStackSize = 1;
                 int maxStackSize = (int)Math.min(minStackSize * (
-                        Math.pow(floorLevel, 0.1D) + 1.0D) * Math.pow(wis.field_76292_a, 0.5D), is
-                        .func_77976_d());
+                        Math.pow(floorLevel, 0.1D) + 1.0D) * Math.pow(wis.itemWeight, 0.5D), is
+                        .getMaxStackSize());
                 int stackSize = minStackSize + rnd.nextInt(maxStackSize - minStackSize + 1);
-                is.field_77994_a = stackSize;
+                is.stackSize = stackSize;
                 ((List)list).add(is);
             }
         }
@@ -150,24 +151,24 @@ public class WorldGenDroneTower
 
     public static boolean genDrones(World world, BlockPos bp1, BlockPos bp2, int floorLevel)
     {
-        BlockPos pos1 = new BlockPos(Math.min(bp1.func_177958_n(), bp2.func_177958_n()), Math.min(bp1.func_177956_o(), bp2.func_177956_o()), Math.min(bp1.func_177952_p(), bp2.func_177952_p()));
+        BlockPos pos1 = new BlockPos(Math.min(bp1.getX(), bp2.getX()), Math.min(bp1.getY(), bp2.getY()), Math.min(bp1.getZ(), bp2.getZ()));
 
-        BlockPos pos2 = new BlockPos(Math.max(bp1.func_177958_n(), bp2.func_177958_n()), Math.max(bp1.func_177956_o(), bp2.func_177956_o()), Math.max(bp1.func_177952_p(), bp2.func_177952_p()));
+        BlockPos pos2 = new BlockPos(Math.max(bp1.getX(), bp2.getX()), Math.max(bp1.getY(), bp2.getY()), Math.max(bp1.getZ(), bp2.getZ()));
         int bigBabyChance = Math.max(5, 200 / (floorLevel + 1));
         Vec3d mid = VecHelper.getMid(new Vec3d[] { new Vec3d(pos1), new Vec3d(pos2) });
-        double xDif = pos2.func_177958_n() - pos1.func_177958_n();
-        double yDif = pos2.func_177956_o() - pos1.func_177956_o();
-        double zDif = pos2.func_177952_p() - pos1.func_177952_p();
+        double xDif = pos2.getX() - pos1.getX();
+        double yDif = pos2.getY() - pos1.getY();
+        double zDif = pos2.getZ() - pos1.getZ();
         if (rnd.nextInt(bigBabyChance) == 0)
         {
             EntityDroneBabyBig bigBaby = new EntityDroneBabyBig(world);
-            bigBaby.func_70107_b(mid.field_72450_a, mid.field_72448_b, mid.field_72449_c);
+            bigBaby.setPosition(mid.xCoord, mid.yCoord, mid.zCoord);
             bigBaby.minionSpawnRange = Math.min(xDif / 2.0D, Math.min(yDif, zDif / 2.0D));
             bigBaby.hostile = true;
             bigBaby.shouldDespawn = false;
-            bigBaby.field_98038_p = true;
+            bigBaby.forceSpawn = true;
             bigBaby.onInitSpawn();
-            world.func_72838_d(bigBaby);
+            world.spawnEntity(bigBaby);
             return true;
         }
         int babyCount = 2 + rnd.nextInt(3 + floorLevel * 2);
@@ -175,13 +176,13 @@ public class WorldGenDroneTower
         {
             EntityDroneBaby baby = new EntityDroneBaby(world);
 
-            Vec3d pos = new Vec3d(pos1.func_177958_n() + rnd.nextDouble() * (pos2.func_177958_n() - pos1.func_177958_n()), pos1.func_177956_o() + rnd.nextDouble() * (pos2.func_177956_o() - pos1.func_177956_o()), pos1.func_177952_p() + rnd.nextDouble() * (pos2.func_177952_p() - pos1.func_177952_p()));
-            baby.func_70107_b(pos.field_72450_a, pos.field_72448_b, pos.field_72449_c);
+            Vec3d pos = new Vec3d(pos1.getX() + rnd.nextDouble() * (pos2.getX() - pos1.getX()), pos1.getY() + rnd.nextDouble() * (pos2.getY() - pos1.getY()), pos1.getZ() + rnd.nextDouble() * (pos2.getZ() - pos1.getZ()));
+            baby.setPosition(pos.xCoord, pos.yCoord, pos.zCoord);
             baby.hostile = true;
             baby.shouldDespawn = false;
-            baby.field_98038_p = true;
+            baby.forceSpawn = true;
             baby.onInitSpawn();
-            world.func_72838_d(baby);
+            world.spawnEntity(baby);
         }
         return false;
     }
