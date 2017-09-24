4746d9369836836be01ced1311680290c8120e43
Ayaka Neko <neko@ayaka.moe>
2017-09-24 19:21:08 +0800

    New event added following up with the Item and CreativeTabs update

--- src/main/java/com/github/nekomeowww/customdrones/common/CommonProxy.java
+++ src/main/java/com/github/nekomeowww/customdrones/common/CommonProxy.java
@@ -3,12 +3,16 @@ package com.github.nekomeowww.customdrones.common;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.github.nekomeowww.customdrones.item.ItemLoader;
+
+import com.github.nekomeowww.customdrones.creativetab.CreativeTabsLoader;
 
 public class CommonProxy
 {
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        new ItemLoader(event);
+        new CreativeTabsLoader(event);
     }
 
     public void init(FMLInitializationEvent event)
